197707da74ab79d9088e162809d5f4ef735ee247
KauaneDelvoss <delvoss.ribas@gmail.com>
2021-12-17 00:25:13 -0300

    formatação template

--- src/views/Perfil.vue
+++ src/views/Perfil.vue
@@ -1,88 +1,58 @@
 <template>
-  <v-container class="text-center pa-8 fluid">
-      <h2 class="h2">MEU PERFIL</h2>
-      <v-form
-        ref="form"
-        v-model="valid"
-        lazy-validation
-      >
-        <v-text-field
-          v-model="name"
-          :counter="10"
-          :rules="nameRules"
-          label="Name"
-          required
-        ></v-text-field>
-
-        <v-text-field
-          v-model="email"
-          :rules="emailRules"
-          label="E-mail"
-          required
-        ></v-text-field>
-
-        
-
-        
-
-        <v-btn
-          :disabled="!valid"
-          color="success"
-          class="mr-4"
-          @click="validate"
-        >
-          Validate
-        </v-btn>
-
-        <v-btn
-          color="error"
-          class="mr-4"
-          @click="reset"
-        >
-          Reset Form
-        </v-btn>
-
-        <v-btn
-          color="warning"
-          @click="resetValidation"
-        >
-          Reset Validation
-        </v-btn>
-      </v-form>
+  <v-container class="pa-8" fluid>
+    <h1 class="h1">Meu perfil</h1>
+    <v-form>
+      <v-container>
+        <v-text-field label="Nome" v-model="nome"></v-text-field>
+        <v-text-field label="Sobrenome" v-model="sobrenome"></v-text-field>
+        <v-btn color="primary" @click="salvarPerfil">Salvar</v-btn>
+      </v-container>
+    </v-form>
   </v-container>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      valid: true,
-      name: '',
-      nameRules: [
-        v => !!v || 'Name is required',
-        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-      ],
-      email: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-      ],
-      
-    }),
-
-    methods: {
-      validate () {
-        this.$refs.form.validate()
-      },
-      reset () {
-        this.$refs.form.reset()
-      },
-      resetValidation () {
-        this.$refs.form.resetValidation()
-      },
+import * as fb from "@/plugins/firebase";
+export default {
+  data() {
+    return {
+      nome: "",
+      sobrenome: "",
+      uid: "",
+      temPerfil: false,
+    };
+  },
+  async mounted() {
+    this.uid = fb.auth.currentUser.uid;
+    const userProfile = await fb.profileCollection
+      .where("uid", "==", this.uid)
+      .get();
+    if (userProfile.docs.length > 0) {
+      this.temPerfil = true;
+      const perfil = userProfile.docs[0];
+      this.profileId = perfil.id;
+      this.nome = perfil.data().nome;
+      this.sobrenome = perfil.data().sobrenome;
+    }
+  },
+  methods: {
+    async salvarPerfil() {
+      if (this.temPerfil) {
+        await fb.profileCollection.doc(this.profileId).update({
+          nome: this.nome,
+          sobrenome: this.sobrenome,
+        });
+      } else {
+        await fb.profileCollection.add({
+          uid: this.uid,
+          nome: this.nome,
+          sobrenome: this.sobrenome,
+        });
+      }
     },
-  }
+  },
+};
 </script>
 
 <style>
-
 </style>
